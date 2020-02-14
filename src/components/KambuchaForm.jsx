e572e1f391fccc8ced870dd309c3ab32aa8e4292
Student <example@example.com>
2020-02-14 14:07:37 -0800

    revert back to old commit once again, webpage loads onto local server.

--- src/components/KambuchaForm.jsx
+++ src/components/KambuchaForm.jsx
@@ -1,59 +1,59 @@
-// import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
-// import TextField from '@material-ui/core/TextField';
-// import Card from '@material-ui/core/Card';
-// import CardActions from '@material-ui/core/CardActions';
-// import CardContent from '@material-ui/core/CardContent';
-// import Button from '@material-ui/core/Button';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
 
-// const moveCard = {
-//   paddingLeft:'300px'
-// };
+const moveCard = {
+  paddingLeft:'300px'
+};
 
-// const useStyles = makeStyles({
-//   root: {
-//     minWidth: 365,
-//     width: '170px',
-//     marginTop: 100,
-//     height: '500px',
-//   },
-//   title: {
-//     fontSize: 14,
+const useStyles = makeStyles({
+  root: {
+    minWidth: 365,
+    width: '170px',
+    marginTop: 100,
+    height: '500px',
+  },
+  title: {
+    fontSize: 14,
       
-//   },
-//   pos: {
-//     marginBottom: 1,
+  },
+  pos: {
+    marginBottom: 1,
       
-//   },
-// });
+  },
+});
 
 
 
-// function KambuchaForm() {
-//   const classes = useStyles();
+function KambuchaForm() {
+  const classes = useStyles();
 
-//   return (
-//     <div style={moveCard}>
-//       <Card className={classes.root} variant="outlined">
-//         <CardContent>
-//           <form>
-//             <h3>Employee Order.</h3>
-//             <TextField id="filled-basic" label="Name" variant="filled" />
-//             <br/><br/>
-//             <h5>Check-In</h5>
-//             <TextField id="filled-basic" label="Brand" variant="filled" />
-//             <br/>
-//             <h5>Guests</h5>
-//             <TextField id="filled-basic" label="Flavor" variant="filled" />
-//           </form> 
-//         </CardContent>
-//         <CardActions>
-//           <Button   variant="contained" size="medium" color="primary">Submit</Button>
-//         </CardActions>
-//       </Card>
+  return (
+    <div style={moveCard}>
+      <Card className={classes.root} variant="outlined">
+        <CardContent>
+          <form>
+            <h3>Employee Order.</h3>
+            <TextField id="filled-basic" label="Name" variant="filled" />
+            <br/><br/>
+            <h5>Check-In</h5>
+            <TextField id="filled-basic" label="Brand" variant="filled" />
+            <br/>
+            <h5>Guests</h5>
+            <TextField id="filled-basic" label="Flavor" variant="filled" />
+          </form> 
+        </CardContent>
+        <CardActions>
+          <Button   variant="contained" size="medium" color="primary">Submit</Button>
+        </CardActions>
+      </Card>
  
    
-//     </div>
-//   );
-// }
-// export default KambuchaForm;
+    </div>
+  );
+}
+export default KambuchaForm;
