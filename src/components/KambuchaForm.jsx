8fd51adb5147ae172858829250045bbfd9bec54a
Student <example@example.com>
2020-02-14 11:42:50 -0800

    add form for employee, menu.

--- src/components/KambuchaForm.jsx
+++ src/components/KambuchaForm.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
+// import Card from '@material-ui/core/Card';
+// import CardActions from '@material-ui/core/CardActions';
+// import CardContent from '@material-ui/core/CardContent';
+// import Button from '@material-ui/core/Button';
 
 const moveCard = {
   paddingLeft:'300px'
