b8ae3ae2605dde35db75ed89eb23c6afc4f6e8a8
Student <example@example.com>
2020-02-21 10:16:30 -0800

    will add refs to form.

--- src/components/KambuchaForm.jsx
+++ src/components/KambuchaForm.jsx
@@ -10,6 +10,67 @@ const moveCard = {
   paddingLeft:'400px',
 };
 
+
+function KambuchaForm() {
+
+  let _name = null;
+  let _brand = null;
+  let _flavor = null;
+  let _quantity = null;
+
+function handleNewTicketFormSubmission(event) {
+  event.preventDesault();
+  _name.value = '';
+  _brand.value = '';
+  _flavor.value = '';
+  _quantity.value = '';
+
+}
+
+  // const classes = useStyles();
+  return (
+    <div style={moveCard}>
+      <Card variant="outlined">
+        <CardContent>
+          <form onSubmit={handleNewTicketFormSubmission}>
+            <h3>Employee Order.</h3>
+            <TextField 
+            type='text'
+            id="filled-basic" 
+            label="Name" 
+            variant="filled" 
+            ref={(input) => {_name = input;}}/>/>
+            <br/><br/>
+            <TextField 
+            type='text'
+            id="filled-basic" 
+            label="Brand"
+             variant="filled"
+             ref={(input) => {_brand = input;}}/> />
+            <br/><br/>
+            <TextField 
+            type='text'
+            id="filled-basic"
+             label="Flavor"
+              variant="filled"
+              ref={(input) => {_flavor = input;}}/> />
+            <br/><br/>
+            <TextField
+            type='text'
+            id="filled-basic"
+            label="Quantity"
+             variant="filled" 
+             ref={(input) => {_quantity = input;}}/>/>
+          </form> 
+        </CardContent>
+        <CardActions>
+          <Button   variant="contained" size="medium" color="primary">Submit</Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
+}
+
 // const useStyles = makeStyles({
 //   root: {
 //     minWidth: 360,
@@ -26,52 +87,6 @@ const moveCard = {
       
 //   },
 // });
-
-
-
-function KambuchaForm() {
-  // const classes = useStyles();
-
-  return (
-    <div style={moveCard}>
-      <Card variant="outlined">
-        <CardContent>
-          <form>
-            <h3>Employee Order.</h3>
-            <TextField 
-            type='text'
-            id="filled-basic" 
-            label="Name" 
-            variant="filled" />
-            <br/><br/>
-            <TextField 
-            type='text'
-            id="filled-basic" 
-            label="Brand"
-             variant="filled" />
-            <br/><br/>
-            <TextField 
-            type='text'
-            id="filled-basic"
-             label="Flavor"
-              variant="filled" />
-            <br/><br/>
-            <TextField
-            type='text'
-            id="filled-basic"
-            label="Quantity"
-             variant="filled" />
-          </form> 
-        </CardContent>
-        <CardActions>
-          <Button   variant="contained" size="medium" color="primary">Submit</Button>
-        </CardActions>
-      </Card>
- 
-   
-    </div>
-  );
-}
 
 
 
