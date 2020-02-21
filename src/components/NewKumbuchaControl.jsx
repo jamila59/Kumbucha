cb32f3cda6fd5979f3dc44e46bb12d663b13bdb6
Student <example@example.com>
2020-02-21 08:00:59 -0800

    add state to project to implement conditional rendering.

---
+++ src/components/NewKumbuchaControl.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { disconnect } from 'cluster';
+
+class NewKumbuchaControl extends React.Component {
+
+    constructor(props)  {
+        super(props);
+        this.state = { 
+            formVisibleOnPage: false
+        };
+    }
+
+    render(){
+        return (
+            <div>
+            </div>
+        );
+    }
+}
